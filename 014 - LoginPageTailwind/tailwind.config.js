c9097da05a2511dd9a74c7d87633d7f76fa52001
Datt Panchal <dattpanchal2904@gmail.com>
2022-05-29 22:24:00 +0530

    Upload files of the 100DaysOfCode!

---
+++ 014 - LoginPageTailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      colors:{
+        "alice-ice": '#e8f0fe',
+        "btngreen": 'rgb(0, 164, 0)',
+        "fbbg": 'rgb(240, 242, 245)',
+      },
+      fontFamily:{
+        "fbfonts": 'SFProDisplay-Regular, Helvetica, Arial, sans-serif',
+        "fbfonts2": 'Helvetica, Arial, sans-serif',
+      }
+
+    },
+  },
+  plugins: [],
+}
